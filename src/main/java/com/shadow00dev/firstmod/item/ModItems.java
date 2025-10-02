8ad984a77302611479106b10dab0b58bf1d2fa2d
shadow00dev <twistcharlievlogs@gmail.com>
2025-10-02 18:59:20 +0100

    It works now

--- src/main/java/com/shadow00dev/firstmod/item/ModItems.java
+++ src/main/java/com/shadow00dev/firstmod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.shadow00dev.firstmod.item;
 
 import com.shadow00dev.firstmod.FirstMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ToolMaterial;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -9,8 +10,9 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(FirstMod.MODID);
 
-    public static final DeferredItem<Item> FIRST_ITEM = ITEMS.registerSimpleItem(
+    public static final DeferredItem<Item> FIRST_ITEM = ITEMS.registerItem(
             "first_item",
+            props -> new Item(props.sword(ToolMaterial.DIAMOND, 10, -0.4f).durability(1)),
             new Item.Properties());
 
     public static void register(IEventBus eventBus) {
